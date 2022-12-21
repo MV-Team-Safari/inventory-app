670996fa9a8a20bfbac35a0d22f28c8906df71f5
Divya Ranat <38928132+divyaranat@users.noreply.github.com>
2022-12-21 14:36:03 -0800

    Change Sauce render to Items

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { SaucesList } from './SaucesList';
+import { ItemsList } from './ItemsList';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
 
 export const App = () => {
 
-	const [sauces, setSauces] = useState([]);
+	const [items, setItems] = useState([]);
 
 	async function fetchSauces(){
 		try {
-			const response = await fetch(`${apiURL}/sauces`);
+			const response = await fetch(`${apiURL}/items`);
 			const saucesData = await response.json();
 			
-			setSauces(saucesData);
+			setItems(saucesData);
 		} catch (err) {
 			console.log("Oh no an error! ", err)
 		}
@@ -27,7 +27,7 @@ export const App = () => {
 		<main>	
       <h1>Sauce Store</h1>
 			<h2>All things 🔥</h2>
-			<SaucesList sauces={sauces} />
+			<ItemsList items={items} />
 		</main>
 	)
 }
