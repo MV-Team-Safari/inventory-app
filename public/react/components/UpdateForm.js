da82795ec97ed12e5f225735132e32a5f5f0e23e
fraser-clover <leesel.fraser@clover.com>
2023-01-04 03:18:50 -0500

    working on add and image url

--- public/react/components/UpdateForm.js
+++ public/react/components/UpdateForm.js
@@ -1,19 +1,67 @@
 import React, { useState } from "react";
+import apiURL from "../api";
 
-export const UpdateForm = () => {
+export const UpdateForm = (props) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [imgURL, setImgURL] = useState("");
+
+  async function updateItem(e) {
+    e.preventDefault();
+    const data = { title, description, price, category, imgURL };
+    console.log(data);
+
+    const response = await fetch(`${apiURL}/items/${props.individualItem}`, {
+      method: "PUT",
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(data),
+    });
+    return response.json();
+  }
   return (
     <div>
-      <form className="update-form-container">
+      <form className="update-form-container" onSubmit={updateItem}>
         <label>Update Title:</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        ></input>
         <label>Update Description:</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        ></input>
         <label>Update Price:</label>
-        <input type="number"></input>
+        <input
+          type="number"
+          value={price}
+          onChange={(event) => setPrice(event.target.value)}
+        ></input>
         <label>Update Category:</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        ></input>
         <label>Update Image URL:</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          value={imgURL}
+          onChange={(event) => setImgURL(event.target.value)}
+        ></input>
+        <input
+          className="update-form-item-submit-btn"
+          type="submit"
+          value="Update New Item"
+        />
       </form>
     </div>
   );
